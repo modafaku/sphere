70fbab9b74023ebf8dcd2687eeeaacc641222376
modafaku <modafaku@gmail.com>
2024-07-10 14:48:17 -0400

    removesentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 import icon from "astro-icon";
 
@@ -14,7 +13,7 @@ export default defineConfig({
   site: "https://modafaku.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
-  }), sentry(), spotlightjs(), icon()],
+  }), spotlightjs(), icon()],
   output: "server",
   adapter: cloudflare()
 });
