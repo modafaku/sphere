7e52ef4ff96470bc053bbc276b2792274739d090
modafaku <modafaku@gmail.com>
2024-07-10 05:38:35 -0400

    personalized a bit more

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "modafaku",
+  DESCRIPTION: "where my thoughts can play",
+  AUTHOR: "fongu modafaku",
 }
 
 // Work Page
@@ -34,19 +34,19 @@ export const SEARCH: Page = {
 // Links
 export const LINKS: Links = [
   { 
-    TEXT: "Home", 
+    TEXT: "home", 
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
+    TEXT: "work", 
     HREF: "/work", 
   },
   { 
-    TEXT: "Blog", 
+    TEXT: "blog", 
     HREF: "/blog", 
   },
   { 
-    TEXT: "Projects", 
+    TEXT: "projects", 
     HREF: "/projects", 
   },
 ]
@@ -54,25 +54,25 @@ export const LINKS: Links = [
 // Socials
 export const SOCIALS: Socials = [
   { 
-    NAME: "Email",
+    NAME: "email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "modafaku@gmail.com",
+    HREF: "mailto:modafaku@gmail.com",
   },
   { 
-    NAME: "Github",
+    NAME: "github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "modafaku",
+    HREF: "https://github.com/modafaku"
   },
   { 
-    NAME: "LinkedIn",
+    NAME: "linkedin",
     ICON: "linkedin",
     TEXT: "markhorn-dev",
     HREF: "https://www.linkedin.com/in/markhorn-dev/",
   },
   { 
-    NAME: "Twitter",
+    NAME: "twitter",
     ICON: "twitter-x",
     TEXT: "markhorn_dev",
     HREF: "https://twitter.com/markhorn_dev",
