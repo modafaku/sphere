072074f1849d6f977696664d1e26976a263fd85d
modafaku <modafaku@gmail.com>
2024-07-10 14:04:52 -0400

    hiccup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
-
 import icon from "astro-icon";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://modafaku.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
-  }), sentry(), spotlightjs(), icon()]
+  }), sentry(), spotlightjs(), icon()],
+  output: "server",
+  adapter: cloudflare()
 });
